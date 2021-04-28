767a1542528dfdefa9093a3524218db5ce4f6664
RDComeau <danny.comeau@vizientinc.com>
2021-04-28 16:26:09 -0500

    Try Catch added to the Code base. Message now specifies ContentEncoding and ContentType.

--- Program.cs
+++ Program.cs
@@ -16,7 +16,7 @@ int x = 3, y = 3;
 
 string deviceKey = "<deviceKey>";
 string deviceId = "<deviceId>";
-string iotHubHostName = "<IoTHubHostname>";
+string iotHubHostName = "<IoTHubHostname>.azure-devices.net";
 int messageId = 1;
 var deviceAuthentication = new DeviceAuthenticationWithRegistrySymmetricKey(deviceId, deviceKey);
 DeviceClient deviceClient = DeviceClient.Create(iotHubHostName, deviceAuthentication, TransportType.Mqtt);
@@ -24,61 +24,75 @@ DeviceClient deviceClient = DeviceClient.Create(iotHubHostName, deviceAuthentica
 
 while (true)
 {
-    Console.Clear();
-
-    (int dx, int dy, bool holding) = JoystickState(sh);
-
-    if (holding)
+    try
     {
-        n++;
+        Console.Clear();
+
+        (int dx, int dy, bool holding) = JoystickState(sh);
+
+        if (holding)
+        {
+            n++;
+        }
+
+        x = (x + 8 + dx) % 8;
+        y = (y + 8 + dy) % 8;
+
+        sh.Fill(n % 2 == 0 ? Color.DarkBlue : Color.DarkRed);
+        sh.SetPixel(x, y, Color.Yellow);
+
+        var tempValue = sh.Temperature;
+        var temp2Value = sh.Temperature2;
+        var preValue = sh.Pressure;
+        var humValue = sh.Humidity;
+        var accValue = sh.Acceleration;
+        var angValue = sh.AngularRate;
+        var magValue = sh.MagneticInduction;
+        var altValue = WeatherHelper.CalculateAltitude(preValue, defaultSeaLevelPressure, tempValue);
+
+        Console.WriteLine($"Temperature Sensor 1: {tempValue.DegreesCelsius:0.#}\u00B0C");
+        Console.WriteLine($"Temperature Sensor 2: {temp2Value.DegreesCelsius:0.#}\u00B0C");
+        Console.WriteLine($"Pressure: {preValue.Hectopascals:0.##} hPa");
+        Console.WriteLine($"Altitude: {altValue.Meters:0.##} m");
+        Console.WriteLine($"Acceleration: {accValue} g");
+        Console.WriteLine($"Angular rate: {angValue} DPS");
+        Console.WriteLine($"Magnetic induction: {magValue} gauss");
+        Console.WriteLine($"Relative humidity: {humValue.Percent:0.#}%");
+        Console.WriteLine($"Heat index: {WeatherHelper.CalculateHeatIndex(tempValue, humValue).DegreesCelsius:0.#}\u00B0C");
+        Console.WriteLine($"Dew point: {WeatherHelper.CalculateDewPoint(tempValue, humValue).DegreesCelsius:0.#}\u00B0C");
+
+        var telemetryDataPoint = new
+        {
+            messageId = messageId++,
+            deviceId = deviceId,
+            Temp1 = tempValue.ToString(),
+            Temp2 = temp2Value.ToString(),
+            Pressure = preValue.ToString(),
+            Humidity = humValue.ToString(),
+            Acceleration = accValue,
+            AngularRate = angValue,
+            MagneticInduction = magValue,
+            Altitude = altValue.ToString()
+        };
+        string messageString = JsonConvert.SerializeObject(telemetryDataPoint);
+        Message message = new Message(Encoding.ASCII.GetBytes(messageString))
+        {
+            ContentEncoding = "utf-8",
+            ContentType = "application/json"
+        };
+        message.Properties.Add("level", "storage");
+
+        Console.WriteLine("{0} > Sending message: {1}", DateTime.Now, messageString);
+        await deviceClient.SendEventAsync(message);
+        Console.WriteLine("{0} > Sent message: {1}", DateTime.Now, messageString);
+
+        await Task.Delay(1000);
     }
-
-    x = (x + 8 + dx) % 8;
-    y = (y + 8 + dy) % 8;
-
-    sh.Fill(n % 2 == 0 ? Color.DarkBlue : Color.DarkRed);
-    sh.SetPixel(x, y, Color.Yellow);
-
-    var tempValue = sh.Temperature;
-    var temp2Value = sh.Temperature2;
-    var preValue = sh.Pressure;
-    var humValue = sh.Humidity;
-    var accValue = sh.Acceleration;
-    var angValue = sh.AngularRate;
-    var magValue = sh.MagneticInduction;
-    var altValue = WeatherHelper.CalculateAltitude(preValue, defaultSeaLevelPressure, tempValue);
-
-    Console.WriteLine($"Temperature Sensor 1: {tempValue.DegreesCelsius:0.#}\u00B0C");
-    Console.WriteLine($"Temperature Sensor 2: {temp2Value.DegreesCelsius:0.#}\u00B0C");
-    Console.WriteLine($"Pressure: {preValue.Hectopascals:0.##} hPa");
-    Console.WriteLine($"Altitude: {altValue.Meters:0.##} m");
-    Console.WriteLine($"Acceleration: {accValue} g");
-    Console.WriteLine($"Angular rate: {angValue} DPS");
-    Console.WriteLine($"Magnetic induction: {magValue} gauss");
-    Console.WriteLine($"Relative humidity: {humValue.Percent:0.#}%");
-    Console.WriteLine($"Heat index: {WeatherHelper.CalculateHeatIndex(tempValue, humValue).DegreesCelsius:0.#}\u00B0C");
-    Console.WriteLine($"Dew point: {WeatherHelper.CalculateDewPoint(tempValue, humValue).DegreesCelsius:0.#}\u00B0C");
-
-    var telemetryDataPoint = new
+    catch (Exception ex)
     {
-        messageId = messageId++,
-        deviceId = deviceId,
-        Temp1 = tempValue,
-        Temp2 = temp2Value,
-        Pressure = preValue,
-        Humidity = humValue,
-        Acceleration = accValue,
-        AngularRate = angValue,
-        MagneticInduction = magValue,
-        Altitude = altValue
-    };
-    string messageString = JsonConvert.SerializeObject(telemetryDataPoint);
-    Message message = new Message(Encoding.ASCII.GetBytes(messageString));
-
-    await deviceClient.SendEventAsync(message);
-    Console.WriteLine(" Sending message: ", DateTime.Now, messageString);
-
-    await Task.Delay(1000);
+        Console.WriteLine("Message Error");
+        Console.WriteLine(ex.Message.ToString());
+    }
 }
 
 (int, int, bool) JoystickState(SenseHat sh)
